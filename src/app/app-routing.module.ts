0c019ed1af8345d315214a6dcd999661c496ab10
hoangta3061998 <h01694111033@gmail.com>
2019-09-05 10:26:34 +0700

    youtube player

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {TimelinesComponent} from './timelines/timelines.component';
 import {NgModule} from '@angular/core';
+import {YoutubePlayerComponent} from './youtube-player/youtube-player.component';
+import {YoutubePlaylistComponent} from './youtube-playlist/youtube-playlist.component';
 
 const routes: Routes = [
   {
     path: 'timelines',
     component: TimelinesComponent
 
+  },
+  {
+    path: 'youtube',
+    component: YoutubePlaylistComponent,
+    children: [{
+      path: ':id',
+      component: YoutubePlayerComponent
+    }]
   }
 ];
 
